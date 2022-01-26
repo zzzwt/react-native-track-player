5af724d55ea72b83411d6b7ecd6226aaa4c1f25e
zwt <zhangweitao@mockuai.com>
2022-01-26 11:42:00 +0800

    修改错误的objc参数

--- ios/RNTrackPlayer/RNTrackPlayer.swift
+++ ios/RNTrackPlayer/RNTrackPlayer.swift
@@ -501,7 +501,7 @@ public class RNTrackPlayer: RCTEventEmitter {
         }
     }
 
-    @objc
+    @objc(getDuration)
     public func getDuration(resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
         resolve(player.duration)
     }
